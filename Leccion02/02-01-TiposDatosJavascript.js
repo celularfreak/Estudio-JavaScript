d35d0c0988d84964db149fb1c61c8df552fa44e2
Celularfreak <celularfreak@hotmail.com>
2022-06-17 12:11:02 +0200

    Leccion2

---
+++ Leccion02/02-01-TiposDatosJavascript.js
@@ -0,0 +1,20 @@
+/*
+Ejemplos de
+tipos de datos
+en JavasCript
+*/
+//Tipo de dato string
+var nombre = "Carlos";
+console.log(nombre);
+
+//Tipo de dato numerioo
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objeto = {
+	nombre : "Juan",
+	apellido : "Perez",
+	telefono : "55443322"
+}
+console.log(objeto);
